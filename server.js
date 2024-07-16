64f7fa5f27a2ef585b50d5f543b1f64adad796d8
TannerHadley <thadley11@ivytech.edu>
2024-07-16 12:16:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const PORT = process.env.PORT || 3030;
 
 app.use(express.static(path.join(__dirname)));
 
-// register view engine
-app.set('view engine', 'ejs');
-
-// middleware & static files
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-
 app.use('/', (req, res) => {
   res.redirect('/views')
 });
