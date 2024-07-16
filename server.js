6cd3334d288af35ed646c47a08bd6f4fe9707c3f
TannerHadley <thadley11@ivytech.edu>
2024-07-16 12:59:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-app.use('/views');
-
+app.get('/course_create', (req, res) => {
+    res.render('course_create.ejs', { title: 'Add a new course'});
+});
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
