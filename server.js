bc3a556ce5304730896636807767dead33cf957a
TannerHadley <thadley11@ivytech.edu>
2024-07-16 12:32:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-app.use(express.static(path.join(__dirname)));
-
 app.get('/', (req, res) => {
   res.redirect('/views')
 });
