5186ce8aa69248006ecd32ecce282a1cab535e50
TannerHadley <thadley11@ivytech.edu>
2024-07-16 12:10:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.use('/', (req, res) => {
   res.redirect('/views')
 });
 
